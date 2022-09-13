96a74b19c7d63e7a18acc1b4b97dc2c57dda3620
sujanbaniksec <65958992+sujanbaniksec@users.noreply.github.com>
2022-09-13 17:03:57 -0400

    upgrade

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,21 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   defaultCommandTimeout: 30000,
   retries: { openMode: 2, runMode: 2 },
+  video: false,
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Seczetta E2E Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    autoOpen: true,
+    overwrite: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
     excludeSpecPattern: [
       "**/cypress/e2e/1-getting-started",
